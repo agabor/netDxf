67c1c7c9cc69aac5e25873f0aa67725ed6657aae
Gábor Angyal <gabor.angyal@codesharp.hu>
2021-08-03 16:47:32 +0200

    sample

---
+++ sample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using netDxf;
+using netDxf.Blocks;
+using netDxf.Collections;
+using netDxf.Entities;
+using netDxf.Header;
+using netDxf.Objects;
+using netDxf.Tables;
+using netDxf.Units;
+
+namespace sample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // your DXF file name
+            string file = "sample.dxf";
+
+            // create a new document, by default it will create an AutoCad2000 DXF version
+            DxfDocument doc = new DxfDocument();
+            // an entity
+            //Line entity = new Line(new Vector2(5, 5), new Vector2(10, 5));
+            // add your entities here
+            //doc.Entities.Add(entity);
+            // save to file
+            doc.Save(file);
+
+            // this check is optional but recommended before loading a DXF file
+            DxfVersion dxfVersion = DxfDocument.CheckDxfFileVersion(file);
+            // netDxf is only compatible with AutoCad2000 and higher DXF versions
+            if (dxfVersion < DxfVersion.AutoCad2000) return;
+            // load file
+            DxfDocument loaded = DxfDocument.Load(file);
+        }
+    }
+}
